f2b146bd9a1f5b66560dec02f4a076f29309b7b1
fhunter06 <77547718+fhunter06@users.noreply.github.com>
2021-01-23 17:58:37 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void AddMusteri(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Isim + " Adlı Müşteri Eklendi");
+        }
+
+        public void ShowMusteri(Musteri musteri)
+        {
+            Console.WriteLine("Id : "+musteri.Id );
+            Console.WriteLine("İsim : " + musteri.Isim);
+            Console.WriteLine("Soyisim : "+musteri.Soyisim);
+            Console.WriteLine("Müşteri No : "+musteri.MusteriNo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = "Galatasaray";
+            musteri1.Isim = "Fernando";
+            musteri1.Soyisim = "Muslera";
+            musteri1.MusteriNo = 1;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = "Galatasaray";
+            musteri2.Isim = "Arda";
+            musteri2.Soyisim = "Turan";
+            musteri2.MusteriNo = 66;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = "Galatasaray";
+            musteri3.Isim = "Fatih";
+            musteri3.Soyisim = "Terim";
+            musteri3.MusteriNo = 10;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+
+            foreach (Musteri  musteri in musteriler)
+            {
+                musteriManager.AddMusteri(musteri);
+            }
+
+            Console.WriteLine(".........................");
+
+
+
+            foreach (Musteri musteri in musteriler)   
+            {
+                musteriManager.ShowMusteri(musteri);
+                Console.WriteLine("........................");
+            }
+        }
+    }
+}
